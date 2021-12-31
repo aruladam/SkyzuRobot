85e77a61a0f289cbd900149e59ce97095b796444
aruladam <95761288+aruladam@users.noreply.github.com>
2021-12-31 18:39:16 +0000

    Update __main__.py

--- SkyzuRobot/__main__.py
+++ SkyzuRobot/__main__.py
@@ -81,7 +81,6 @@ def get_readable_time(seconds: int) -> str:
 
 
 PM_START_TEXT = """
-*Hello {} !*
 [✨](https://telegra.ph/file/601f4266ccc688aea01ee.jpg) ʜᴇʟʟᴏ  ᴍʏ ɴᴀᴍᴇ ɪs ɴᴏɪssʏʀᴏʙᴏᴛ
 ɪ'ᴍ ᴀ ᴀɴɪᴍᴇ ᴛʜᴇᴍᴇ ʙᴏᴛ ꜰᴏʀ ᴍᴀɴᴀɢᴇᴍᴇɴᴛ ʏᴏᴜʀ ɢʀᴏᴜᴘ ᴇᴀsɪʟʏ!
 ᴍᴀɪɴᴛᴀɪɴᴇᴅ ʙʏ [HALBERT](https://t.me/SkyiArul) 
